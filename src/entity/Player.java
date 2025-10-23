f9770dfc28ab6077d33f992c0c162dd96f39f66b
Ariana de Freitas <a.b.d.freitas@student.tue.nl>
2025-10-23 16:44:14 +0200

    (feat): changed map to a more fun and engaging open world setting + fixing asset positions

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -99,8 +99,8 @@ public class Player extends Entity {
 
     public void setDefaultValues() {
 
-        worldX = gp.tileSize * 23;
-        worldY = gp.tileSize * 21;
+        worldX = gp.tileSize * 25;
+        worldY = gp.tileSize * 30;
         speed = 0;
         speedMax = 4;
         direction = "down";
--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Game title");
+        window.setTitle("Bloom or Doom");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
